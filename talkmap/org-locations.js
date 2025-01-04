b32cba234139d5146c4f301d26e9036e8d4b0e20
clearya1 <65236277+clearya1@users.noreply.github.com>
2025-01-04 14:30:06 +0000

    update Jan 2025

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -13,15 +13,15 @@ var addressPoints = [
     "Cannes, France",
     43.55085902980104, 
     7.0188419614579205
-  ]
+  ],
   [
     "Baltimore, USA",
     39.289662855257376, -76.61626048664974
-  ]
+  ],
   [
     "Johns Hopkins University, USA",
      39.329831, -76.620520
-  ]
+  ],
   [
     "Salt Lake City, USA",
     40.76806956002251, -111.8961246455115
